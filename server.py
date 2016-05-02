e94fde5f9a9a48149887d73b0b537d62917fea82
Teddy <m.fahrazeev@gmail.com>
2016-05-02 11:39:45 +0300

    3 commit

--- server.py
+++ server.py
@@ -6,13 +6,11 @@ s.bind(('0.0.0.0', 2222))
 s.listen(10)                                                                    
 for i in range(10) :                                                             
    pid=os.fork()                                                                
-                                                       
    try:                                                                         
         while True:                                                             
               conn, addr = s.accept()                                           
-                                                                     
-                 data = conn.recv(1024)                                         
-                 if not data:                                              
-                 conn.send(data)                                                
-                 if data=='close':                                        
-                 conn.close()    
+              data = conn.recv(1024)                                         
+              if not data:                                              
+              conn.send(data)                                                
+              if data=='close':                                        
+              conn.close()    
